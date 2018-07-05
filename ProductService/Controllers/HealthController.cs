8f16a43f0bfc970cddc56e73fbe68297aec7f401
xujie <xujie@flexem.cn>
2018-07-05 09:09:46 +0800

    save

---
+++ ProductService/Controllers/HealthController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductService.Controllers
+{
+    [Route("api/[controller]")]
+    public class HealthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Console.WriteLine("健康检查" + DateTime.Now);
+            return Ok("ok");
+        }
+    }
+}
